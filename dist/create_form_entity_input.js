1a1eaff011ee7efdf353f7c62980884437e90580
Marco <macrozone@gmail.com>
2017-06-27 13:28:05 +0200

    add draggable

--- dist/create_form_entity_input.js
+++ dist/create_form_entity_input.js
@@ -12,6 +12,10 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _reactDraggable = require('react-draggable');
+
+var _reactDraggable2 = _interopRequireDefault(_reactDraggable);
+
 var _dialog_entity = require('./components/dialog_entity');
 
 var _dialog_entity2 = _interopRequireDefault(_dialog_entity);
@@ -26,7 +30,11 @@ exports.default = function (_ref) {
   var schema = _ref.schema;
 
   var FormEntityInput = function FormEntityInput(props) {
-    return _react2.default.createElement(_dialog_entity2.default, (0, _extends3.default)({}, props, { schema: (0, _wrap_in_simpleschema2.default)(schema) }));
+    return _react2.default.createElement(
+      _reactDraggable2.default,
+      null,
+      _react2.default.createElement(_dialog_entity2.default, (0, _extends3.default)({}, props, { schema: (0, _wrap_in_simpleschema2.default)(schema) }))
+    );
   };
   FormEntityInput.displayName = 'FormEntityInput';
   return FormEntityInput;
--- src/create_form_entity_input.js
+++ src/create_form_entity_input.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Draggable from 'react-draggable';
 
 import DialogEntity from './components/dialog_entity';
 import wrapInSimpleSchema from './utils/wrap_in_simpleschema';
 
+
 export default ({ schema }) => {
   const FormEntityInput = props => (
-    <DialogEntity {...props} schema={wrapInSimpleSchema(schema)} />);
+    <Draggable>
+      <DialogEntity {...props} schema={wrapInSimpleSchema(schema)} />
+    </Draggable>);
   FormEntityInput.displayName = 'FormEntityInput';
   return FormEntityInput;
 };
